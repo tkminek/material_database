a0bdafe257476d47ffdcb4ab2fc37ae9c62c2206
tkminek <kminek.tomas@seznam.cz>
2022-02-15 19:10:32 +0100

    add DC01 data

--- mat_db/main/admin.py
+++ mat_db/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import MaterialType,Material,SnCurve,EnCurve,CyclicCurve
 # Register your models here.
+admin.site.register(MaterialType)
+admin.site.register(Material)
+admin.site.register(SnCurve)
+admin.site.register(EnCurve)
+admin.site.register(CyclicCurve)
