d816273069ea29ce989e2820d890b590de593d9a
tkminek <kminek.tomas@seznam.cz>
2022-02-16 15:41:53 +0100

    add nav+bootstrap

--- mat_db/main/views.py
+++ mat_db/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import MaterialType, Material, SnCurve, EnCurve, CyclicCurve
-
+from itertools import chain
 
 def home(response):
     return render(response, "main/home.html", {})
@@ -20,4 +20,12 @@ def material_list(response, material_type_id):
 
 def material_info(response, material_type_id, material_id):
     material_info = Material.objects.get(pk=material_id)
-    return render(response, "main/material_info.html", {"material_info": material_info})
+    c_curve=CyclicCurve.objects.filter(material_id=material_id)
+    en_curve = EnCurve.objects.filter(material_id=material_id)
+    sn_curve = SnCurve.objects.filter(material_id=material_id)
+    curve_ls=list(chain(c_curve,en_curve,sn_curve))
+    return render(response, "main/material_info.html", {
+        "material_info": material_info,
+        "curve_ls": curve_ls,
+    })
+
