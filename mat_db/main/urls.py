156901441042dd2da3b90eb2f93aa596d25ec3b4
tkminek <kminek.tomas@seznam.cz>
2022-02-16 19:03:34 +0100

    add curve info

--- mat_db/main/urls.py
+++ mat_db/main/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('material_type_list/', views.material_type_list, name="material_type_list"),
-    path('material_type_list/<int:material_type_id>', views.material_list, name="material_list"),
-    path('material_type_list/<int:material_type_id>/<int:material_id>', views.material_info, name="material_info"),
+    path('material_type_list/<int:material_type_id>', views.material_list, name= "material_list"),
+    path('material_type_list/<int:material_type_id>/<int:material_id>', views.material_info, name= "material_info"),
+    path('material_type_list/<int:material_type_id>/<int:material_id>/<str:curve_name>', views.curve_info, name= "curve_info"),
 ]
--- mat_db/main/views.py
+++ mat_db/main/views.py
@@ -19,13 +19,42 @@ def material_list(response, material_type_id):
 
 
 def material_info(response, material_type_id, material_id):
+    material_type = MaterialType.objects.get(pk=material_type_id)
     material_info = Material.objects.get(pk=material_id)
     c_curve=CyclicCurve.objects.filter(material_id=material_id)
     en_curve = EnCurve.objects.filter(material_id=material_id)
     sn_curve = SnCurve.objects.filter(material_id=material_id)
     curve_ls=list(chain(c_curve,en_curve,sn_curve))
     return render(response, "main/material_info.html", {
+        "material_type": material_type,
         "material_info": material_info,
         "curve_ls": curve_ls,
     })
 
+
+def curve_info(response, material_type_id, material_id, curve_name):
+    material_type = MaterialType.objects.get(pk=material_type_id)
+    material_info = Material.objects.get(pk=material_id)
+    if CyclicCurve.objects.filter(name=curve_name).exists():
+        curve_info = CyclicCurve.objects.get(material_id=material_id)
+        return render(response, "main/cyclic_curve.html", {
+            "material_type": material_type,
+            "curve_info": curve_info,
+            "material_info": material_info,
+        })
+    elif EnCurve.objects.filter(name=curve_name).exists():
+        curve_info = EnCurve.objects.get(material_id=material_id)
+        return render(response, "main/en_curve.html", {
+            "material_type": material_type,
+            "curve_info": curve_info,
+            "material_info": material_info,
+        })
+    elif SnCurve.objects.filter(name=curve_name).exists():
+        curve_info = SnCurve.objects.get(material_id=material_id)
+        return render(response, "main/sn_curve.html", {
+            "material_type": material_type,
+            "curve_info": curve_info,
+            "material_info": material_info,
+        })
+    else:
+        return HttpResponse("chyba")
